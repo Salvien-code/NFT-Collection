4d2a70bc8079e0ce7225dc1494203dd0bdb486d0
Simon Samuel <salviensky@gmail.com>
2022-10-27 02:40:54 +0100

    Started presale

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from "next";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "../styles/Home.module.css";
-import { Contract, utils } from "ethers";
+import { Contract, providers, Signer, utils } from "ethers";
+import Web3Modal from "web3modal";
 import {
   NFT_CONTRACT_ABI,
   NFT_CONTRACT_ADDRESS,
 } from "../../smart-contracts/Constants";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   const [walletConnected, setWallletConnected] = useState(false);
@@ -15,7 +18,7 @@ const Home: NextPage = () => {
   const [loading, setLoading] = useState(false);
   const [isOwner, setIsOwner] = useState(false);
   const [tokenIdsMinted, setTokenIdsMinted] = useState("0");
-  const web3ModalRef = useRef();
+  const web3ModalRef = useRef<Web3Modal>();
 
   const presaleMint = async () => {
     try {
@@ -87,8 +90,216 @@ const Home: NextPage = () => {
       console.error(err);
     }
   };
+  const checkIfPresaleStarted = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS,
+        NFT_CONTRACT_ABI,
+        provider
+      );
+      const _presaleStarted = await nftContract.presaleStarted();
+      if (!_presaleStarted) {
+        await getOwner();
+      }
+      setPresaleStarted(_presaleStarted);
+      return _presaleStarted;
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
+  };
 
-  return <div></div>;
+  const checkIfPresaleEnded = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS,
+        NFT_CONTRACT_ABI,
+        provider
+      );
+
+      const _presaleEnded = await nftContract.presaleEnded();
+      const hasEnded = _presaleEnded.lt(Math.floor(Date.now() / 1000));
+
+      if (hasEnded) {
+        setPresaleEnded(true);
+      } else {
+        setPresaleEnded(false);
+      }
+      return hasEnded;
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
+  };
+
+  const getOwner = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS,
+        NFT_CONTRACT_ABI,
+        provider
+      );
+      const _owner = await nftContract.owner();
+      const signer = (await getProviderOrSigner(true)) as Signer;
+      const address = await signer.getAddress();
+      if (address.toLowerCase() === _owner.toLowerCase()) {
+        setIsOwner(true);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const getTokenIdsMinted = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS,
+        NFT_CONTRACT_ABI,
+        provider
+      );
+      const _tokenIds = await nftContract.tokenIds();
+      setTokenIdsMinted(_tokenIds.toString());
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    const provider = await web3ModalRef.current!.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 5) {
+      window.alert("Change the network to Goerli");
+      throw new Error("Change network to Goerli");
+    }
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+
+    return web3Provider;
+  };
+
+  useEffect(() => {
+    if (!walletConnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "goerli",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+
+      const _presaleStarted = checkIfPresaleStarted();
+
+      if (_presaleStarted) {
+        checkIfPresaleEnded();
+        console.log("checking");
+      }
+
+      getTokenIdsMinted();
+
+      const presaleEndedInterval = setInterval(async function () {
+        const _presaleStarted = await checkIfPresaleStarted();
+        if (_presaleStarted) {
+          const _presaleEnded = await checkIfPresaleEnded();
+          if (_presaleEnded) {
+            clearInterval(presaleEndedInterval);
+          }
+        }
+      }, 5 * 1000);
+    }
+  }, [walletConnected]);
+
+  const renderButton = () => {
+    if (!walletConnected) {
+      return (
+        <button onClick={connectWallet} className={styles.button}>
+          Connect your wallet
+        </button>
+      );
+    }
+  };
+
+  if (loading) {
+    return <button className={styles.button}>Loading...</button>;
+  }
+
+  if (isOwner && !presaleStarted) {
+    return (
+      <button className={styles.button} onClick={startPresale}>
+        Start Presale!
+      </button>
+    );
+  }
+
+  if (!presaleStarted) {
+    return (
+      <div>
+        <div className={styles.description}>Presale has not started!</div>
+      </div>
+    );
+  }
+
+  if (presaleStarted && !presaleEnded) {
+    return (
+      <div>
+        <div className={styles.description}>
+          Presale has started!!! If your address is whitelisted. Mint a Lion!
+        </div>
+        <button className={styles.button} onClick={presaleMint}>
+          Presale Mint
+        </button>
+      </div>
+    );
+  }
+
+  if (presaleEnded && presaleStarted) {
+    return (
+      <button className={styles.button} onClick={publicMint}>
+        Public Mint
+      </button>
+    );
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Lion NFT Collection</title>
+        <meta name="description" content="Whitelist Dapp" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}>Welcome to the Lion Factory!</h1>
+          <div className={styles.description}>
+            It is the best NFT collection EVER!!!
+          </div>
+          <div className={styles.description}>
+            {tokenIdsMinted}/30 have been minted
+          </div>
+          {renderButton()}
+        </div>
+
+        <div>
+          <Image
+            className={styles.image}
+            src="./Lions/0.svg"
+            alt="The lions SVG"
+          />
+        </div>
+      </div>
+
+      <footer className={styles.footer}>
+        Made with &#100084 by Simon Samuel
+      </footer>
+    </div>
+  );
 };
-
 export default Home;
